62774ed8754b5a76a511bc596b059cbdb4166809
Etienne Cordonnier <cordonnier.etienne@gmail.com>
2014-09-03 22:44:44 +0200

    Remote Coocox project. Add Keil project.
    There was a frame-rate problem with the Coocox Project.
    Ahmed started a new Keil Project, which reaches faster frame-rates.

---
+++ user/graphics.c
@@ -0,0 +1,406 @@
+#include <math.h>
+
+#include "graphics.h"
+
+
+// precomputed sin table * 256
+static const short tsin[91]= { 0,4,9,13,18,22,27,31,36,40,44,49,53,58,62,66,71,75,
+                        79,83,88,92,96,100,104,108,112,116,120,124,128,132,
+                        136,139,143,147,150,154,158,161,165,168,171,175,178,
+                        181,184,187,190,193,196,199,202,204,207,210,212,215,
+                        217,219,222,224,226,228,230,232,234,236,237,239,241,
+                        242,243,245,246,247,248,249,250,251,252,253,254,254,
+                        255,255,255,256,256,256,256
+                        };
+
+
+/**
+ * @brief  Displays a line.
+ * @param  Xpos: specifies the X position.
+ * @param  Ypos: specifies the Y position.
+ * @param  Length: line length.
+ * @param  Direction: line direction.
+ *   This parameter can be one of the following values: LCD_LINE_VERTICAL or Horizontal.
+ * @retval None
+ */
+void Draw_Line(float x1, float y1, float x2, float y2, int color) {
+	float slope;
+	float y;
+	float x;
+	float xdiff = (x2 - x1);
+	float ydiff = (y2 - y1);
+
+	if (xdiff == 0.0f && ydiff == 0.0f) {
+		LCD_WritetoFB((int) x1, (int) y1, color);
+		return;
+	}
+
+	if (fabs(xdiff) > fabs(ydiff)) {
+		float xmin, xmax;
+
+		// set xmin to the lower x value given
+		// and xmax to the higher value
+		if (x1 < x2) {
+			xmin = x1;
+			xmax = x2;
+		} else {
+			xmin = x2;
+			xmax = x1;
+		}
+
+		// draw line in terms of y slope
+		slope = ydiff / xdiff;
+		
+		for (x = xmin; x <= xmax; x += 1.0f) {
+			float y = y1 + ((x - x1) * slope);
+			LCD_WritetoFB((int) x, (int) y, color);
+		}
+	} else {
+		float ymin, ymax;
+
+		// set ymin to the lower y value given
+		// and ymax to the higher value
+		if (y1 < y2) {
+			ymin = y1;
+			ymax = y2;
+		} else {
+			ymin = y2;
+			ymax = y1;
+		}
+
+		// draw line in terms of x slope
+		
+	  slope		= xdiff / ydiff;
+	
+		for ( y = ymin; y <= ymax; y += 1.0f) {
+			float x = x1 + ((y - y1) * slope);
+			LCD_WritetoFB((int) x, (int) y, color);
+		}
+	}
+}
+ 
+
+// this function draws a POLYGON2D based on
+int Draw_Polygon2D(POLYGON2D_PTR poly)
+{
+	// test if the polygon is visible
+	if (poly->state)
+	{
+		unsigned char index;
+		// loop thru and draw a line from vertices 1 to n
+		for (index=0; index < poly->num_verts-1; index++)
+		{
+			// draw line from ith to ith+1 vertex
+			Draw_Line(poly->vlist[index].x + poly->x0,
+					poly->vlist[index].y + poly->y0,
+					poly->vlist[index + 1].x + poly->x0,
+					poly->vlist[index + 1].y + poly->y0, poly->color);
+		} // end for
+
+		// now close up polygon
+		// draw line from last vertex to 0th
+		Draw_Line(poly->vlist[0].x + poly->x0, poly->vlist[0].y + poly->y0,
+				poly->vlist[7].x + poly->x0, poly->vlist[7].y + poly->y0,
+				poly->color);
+
+		// return success
+		return (1);
+	} // end if
+	else
+		return(0);
+}
+
+// this function translates the center of a polygon
+int Translate_Polygon2D(POLYGON2D_PTR poly, int dx, int dy)
+{
+	// test for valid pointer
+	if (!poly)
+		return (0);
+
+	// translate
+	poly->x0 += dx;
+	poly->y0 += dy;
+
+	// return success
+	return (1);
+}
+
+
+// this function rotates the local coordinates of the polygon
+int Rotate_Polygon2D(POLYGON2D_PTR poly, int theta)
+{
+	unsigned char curr_vert;
+	float theta_Rad;
+	
+	// test for valid pointer
+	if (!poly)
+	   return(0);
+
+	
+	theta_Rad= (float)theta*3.14159 / 180; // trig functions work in rad
+
+	
+	// loop and rotate each point, very crude, no lookup!!!
+	for (curr_vert = 0; curr_vert < poly->num_verts; curr_vert++)
+	{
+		// perform rotation
+		float xr = (float) poly->vlist[curr_vert].x * cos(theta_Rad)
+				- (float) poly->vlist[curr_vert].y * sin(theta_Rad);
+
+		float yr = (float) poly->vlist[curr_vert].x * sin(theta_Rad)
+				+ (float) poly->vlist[curr_vert].y * cos(theta_Rad);
+
+		// store result back
+		poly->vlist[curr_vert].x = xr;
+		poly->vlist[curr_vert].y = yr;
+
+	} // end for curr_vert
+
+	// return success
+	return(1);
+}
+
+void swap(int *i, int *j) {
+	int t = *i;
+	*i = *j;
+	*j = t;
+}
+
+void DrawSolidTriangle(int x0, int y0, int x1, int y1, int x2, int y2,
+		int color) {
+			
+	
+	float xl_edge;
+	float xr_edge;
+	float dxldy;
+	float dxrdy;
+	float dxdy2;
+	float dxdy1;
+	int d;
+	int y;
+	
+	// Sort our points into order of y
+	// 0 top
+	// 2 middle
+	// 1 bottom
+	if (y1 < y0) {
+		swap(&y1, &y0);
+		swap(&x1, &x0);
+	}
+	if (y2 < y0) {
+		swap(&y2, &y0);
+		swap(&x2, &x0);
+	}
+	if (y1 < y2) {
+		swap(&y2, &y1);
+		swap(&x2, &x1);
+	}
+
+
+	xl_edge	= (float) x0; // left edge
+	
+	xr_edge	= (float) x0; // right edge
+ 
+	dxdy1	= (float) (x2 - x0) / (y2 - y0);
+  dxdy2	= (float) (x1 - x0) / (y1 - y0);
+
+	if (dxdy1 < dxdy2) {
+		dxldy = dxdy1;
+		dxrdy = dxdy2;
+	} else {
+		dxldy = dxdy2;
+		dxrdy = dxdy1;
+	}
+
+	// Top of the triangle
+	for ( d = y0; d < y2; d++) {
+		int x;
+		for (x = xl_edge; x < xr_edge; x++) {
+			LCD_WritetoFB(x, d, color);
+
+		} //end for loop x
+
+		xl_edge = xl_edge + dxldy;
+		xr_edge = xr_edge + dxrdy;
+
+	} // end for loop y
+
+	  // Bottom half of the triangle
+
+	if (dxdy1 < dxdy2) {
+		dxldy = (float) (x2 - x1) / (y2 - y1);
+	} else {
+		dxrdy = (float) (x2 - x1) / (y2 - y1);
+	}
+
+	
+	for ( y = y2; y < y1; y++) {
+		int x;
+		for (x = xl_edge; x < xr_edge; x++) {
+			LCD_WritetoFB(x, y, color);
+		} //end for loop x
+
+		xl_edge = xl_edge + dxldy;
+		xr_edge = xr_edge + dxrdy;
+
+	} // end for loop y
+	
+}
+		
+void Transform_LocalToWorld(POLYGON3D_PTR poly)
+{
+	int curr_vert;
+	if (!poly) return;
+
+
+	for (curr_vert = 0; curr_vert < poly->num_verts; curr_vert++)
+	{
+		poly->world_vlist[curr_vert].x = poly->vlist[curr_vert].x + poly->world_x;
+		poly->world_vlist[curr_vert].y = poly->vlist[curr_vert].y + poly->world_y;
+		poly->world_vlist[curr_vert].z = poly->vlist[curr_vert].z + poly->world_z;
+	}
+}
+
+void Transform_WorldToCamera( POLYGON3D_PTR poly)
+{
+	int curr_vert;
+	// assume camera at 0,0,0 with angles 0,0,0; 
+	for (curr_vert = 0; curr_vert < poly->num_verts; curr_vert++)
+	{
+		poly->camera_vlist[curr_vert].x = poly->vlist[curr_vert].x + poly->world_x;
+		poly->camera_vlist[curr_vert].y = poly->vlist[curr_vert].y + poly->world_y;
+		poly->camera_vlist[curr_vert].z = poly->vlist[curr_vert].z + poly->world_z;
+	}
+}
+
+int Rotate_Polygon3D_YAxis(POLYGON3D_PTR poly, int theta)
+{
+	float theta_Rad = (float)theta*3.14159 / 180; // trig functions work in rad
+  unsigned curr_vert;
+	
+	if (!poly)
+		return 0;
+	
+	// loop and rotate each point, very crude, no lookup!!!
+	for (curr_vert = 0; curr_vert < poly->num_verts; curr_vert++)
+	{
+		// perform rotation
+		float xr = (float)poly->vlist[curr_vert].z*sin(theta_Rad) + (float)poly->vlist[curr_vert].x*cos(theta_Rad);
+		float yr = (float)poly->vlist[curr_vert].y;
+		float zr = (float)poly->vlist[curr_vert].z*cos(theta_Rad) - (float)poly->vlist[curr_vert].x*sin(theta_Rad);
+
+		// store result back
+		poly->vlist[curr_vert].x = xr;
+		poly->vlist[curr_vert].y = yr;
+		poly->vlist[curr_vert].z = zr;
+
+
+	} // end for curr_vert
+	return 1;
+}
+
+int Draw_Polygon3D(POLYGON3D_PTR poly)
+{
+	if (!poly) return 0;
+
+	// test if the polygon is visible
+	if (poly->state)
+	{
+		int index;
+		int vertex; 
+		float x1, y1, z1,     // working variables
+			x2, y2, z2,
+		x3, y3, z3;
+
+		// loop thru and draw a line from vertices 1 to n
+		for (index = 0; index < poly->num_verts - 1; index++)
+		{
+
+			x1 = poly->camera_vlist[index].x;
+			y1 = poly->camera_vlist[index].y;
+			z1 = poly->camera_vlist[index].z;
+
+			x2 = poly->camera_vlist[index+1].x;
+			y2 = poly->camera_vlist[index+1].y;
+			z2 = poly->camera_vlist[index+1].z;
+
+			// project them!
+			x1 = (HALF_SCREEN_WIDTH + x1*viewing_distance / z1);
+			y1 = (HALF_SCREEN_HEIGHT - ASPECT_RATIO*y1*viewing_distance / z1);
+ 
+			x2 = (HALF_SCREEN_WIDTH + x2*viewing_distance / z2);
+			y2 = (HALF_SCREEN_HEIGHT - ASPECT_RATIO*y2*viewing_distance / z2);
+
+
+			// draw line from ith to ith+1 vertex
+			Draw_Line(x1, y1, x2, y2, poly->color);
+
+		} // end for
+
+		x1 = poly->camera_vlist[index].x;
+		y1 = poly->camera_vlist[index].y;
+		z1 = poly->camera_vlist[index].z;
+
+		x2 = poly->camera_vlist[0].x;
+		y2 = poly->camera_vlist[0].y;
+		z2 = poly->camera_vlist[0].z;
+
+		// for now we will only assume drawing 3D Solid Triangle, not a general polygon <-- later
+		x3 = poly->camera_vlist[1].x;
+		y3 = poly->camera_vlist[1].y;
+		z3 = poly->camera_vlist[1].z;
+		
+		// project them!
+		x1 = (HALF_SCREEN_WIDTH + x1*viewing_distance / z1);
+		y1 = (HALF_SCREEN_HEIGHT - ASPECT_RATIO*y1*viewing_distance / z1);
+
+
+		x2 = (HALF_SCREEN_WIDTH + x2*viewing_distance / z2);
+		y2 = (HALF_SCREEN_HEIGHT - ASPECT_RATIO*y2*viewing_distance / z2);
+		
+		
+		x3 = (HALF_SCREEN_WIDTH + x3*viewing_distance / z3);
+		y3 = (HALF_SCREEN_HEIGHT - ASPECT_RATIO*y3*viewing_distance / z3);
+
+	  DrawSolidTriangle(x2, y2, x3, y3, x1, y1, poly->color); // make it solid 
+		// now close up polygon
+		// draw line from last vertex to 0th
+		Draw_Line(x1, y1, x2,y2, poly->color);
+
+		// return success
+		return(1);
+	} // end if
+	else
+		return(0);
+
+}  
+
+int fsin( short alpha)
+{
+    int i = 1;
+
+    // normalize the angle
+    if ( alpha < 0)
+        alpha = 360 - ((-alpha) % 360);
+    else
+        alpha %= 360;
+
+	if ( alpha >= 180)
+	{   // sin(a+180) == - sin(a)
+		alpha -= 180;
+        i = -1;
+    }
+    if ( alpha > 90)
+    { // sin(180-a) == sin (a); when a<180
+        alpha = 180 - alpha;
+    }
+
+    // use the table to approximate the angle
+    return ( i * tsin[ alpha]);
+}
+
+int fcos( short alpha)
+{   // cos(a ) == sin (a+90)
+    return fsin( alpha + 90);
+}
+
---
+++ user/main.c
@@ -0,0 +1,296 @@
+ 
+	/* Includes ------------------------------------------------------------------*/
+	#include "fsmc_sram.h"
+	#include "graphics.h"
+	#include "lcd.h"
+
+	GPIO_InitTypeDef GPIO_InitStructure;
+
+	/* Private function prototypes -----------------------------------------------*/
+	void RCC_Configuration(void);
+	void GPIO_Configuration(void);
+	 
+	void lcd_rst(void);
+	 
+	void Delay(__IO uint32_t nCount);
+	GPIO_InitTypeDef GPIO_InitStructure;
+ 
+
+	volatile uint32_t fractionaltimeunits = 0;
+
+			
+	char TxBuffer[20];
+
+
+	/**
+	 * @brief  Configure the SysTick Base time.
+	 * @param  None
+	 * @retval : None
+	 */
+	void TimingMeasurement_Config(void)
+	{
+		RCC_ClocksTypeDef RCC_ClockFreq;
+
+		/* Get HCLK frequency */
+		RCC_GetClocksFreq(&RCC_ClockFreq);
+
+		SysTick->CTRL &= ~SysTick_CTRL_ENABLE;
+
+		/* Configure HCLK clock as SysTick clock source */
+		SysTick_CLKSourceConfig(SysTick_CLKSource_HCLK_Div8);
+
+		/* Set SysTick Load value to 0xFFFFFF */
+		SysTick->LOAD = 0xFFFFFF;
+
+		/* Disable the SysTick Interrupt */
+		SysTick->CTRL &= ~SysTick_CTRL_TICKINT;
+
+		SysTick->VAL = 0x0;
+	}
+
+	void StartProfiler(void)
+	{
+
+
+		TimingMeasurement_Config();
+		/* Start timing counter: measure display time */
+		SysTick->CTRL |= SysTick_CTRL_ENABLE;
+	}
+
+
+	/**
+	 * @brief  Calculate the number of Ýs in the systick counter.
+	 * @param  None
+	 * @retval : None
+	 */
+	void DisplayTimingCompute(void)
+	{
+		uint32_t counter = SysTick->VAL;
+
+		if (counter != 0)
+		{
+			/* Convert timer ticks to ns */
+			counter =  (0xFFFFFF - counter) * 111;
+		}
+
+		/* Compute timing in microsecond (us) */
+		fractionaltimeunits = counter / 1000;
+
+	}
+
+	void StopProfiler(void)
+	{
+		/* Stop timing counter */
+		SysTick->CTRL &= ~SysTick_CTRL_ENABLE;
+		DisplayTimingCompute();
+
+	}
+
+
+
+	/* Private functions ---------------------------------------------------------*/
+
+	/**
+		* @brief  Main program.
+		* @param  None
+		* @retval : None
+		*/
+	int main(void)
+	{
+			
+		unsigned char index;
+
+		
+	//	POLYGON2D asteroid;
+		POLYGON3D triangle;
+		// define points of asteroid
+//		VERTEX2DF asteroid_vertices[8] = 
+//		{
+//				{33, -3}, {9, -18}, {-12, -9}, {-21, -12},
+//				{-9, 6}, {-15, 15}, {-3, 27}, {21, 21} 
+//		};
+//		
+		
+		 VERTEX3DF triangle_verts[3] = 
+	  { 0, 2, 0, 
+   	 -5, -5, 0,
+	  5, -5, 0
+	  };
+		/* System Clocks Configuration */
+		RCC_Configuration();   
+
+		/* Enable the FSMC Clock */
+		RCC_AHBPeriphClockCmd(RCC_AHBPeriph_FSMC, ENABLE);
+	
+		GPIO_Configuration();
+		
+		/* Configure FSMC Bank1 NOR/PSRAM */
+		FSMC_LCD_Init();
+
+		LCD_Init();
+
+//	
+//		// initialize the asteroid
+//		asteroid.state       = 1;   // turn it on
+//		asteroid.num_verts   = 8;
+//		asteroid.x0          = 180/2; // position it
+//		asteroid.y0          = 140/2;
+//		asteroid.color       = LCD_Blue;
+
+//		for ( index = 0; index <8; index++)
+//		{
+//			asteroid.vlist[index] = asteroid_vertices[index];
+//		}
+		 
+		triangle.state = 1;   // turn it on
+		triangle.num_verts = 3;
+		triangle.x0 = WINDOW_WIDTH/2; // position it
+		triangle.y0 = WINDOW_HEIGHT/2;
+		triangle.color = LCD_Blue;
+		triangle.world_x = 0;
+		triangle.world_y = 0;
+		triangle.world_z = 150; // set world position
+		
+		for ( index = 0; index <3; index++)
+			triangle.vlist[index] = triangle_verts[index];
+	  
+		while (1)
+		{					  
+	 			
+	  StartProfiler();
+		 
+		LCD_ClearFB();		
+		
+	 //  	Draw_Polygon2D(&asteroid);
+	//   	Rotate_Polygon2D(&asteroid, 2);
+			
+	 if(!GPIO_ReadInputDataBit(GPIOE, GPIO_Pin_5))        //S1
+   {
+     triangle.world_z+=1;
+	 }
+
+   if(!GPIO_ReadInputDataBit(GPIOE, GPIO_Pin_4))       //S2
+   {
+		triangle.world_z-=1;
+   }
+			
+  	Rotate_Polygon3D_YAxis(&triangle, 1);
+    Transform_WorldToCamera(&triangle);
+   	Draw_Polygon3D(&triangle);
+			
+		LCD_Flip();	
+ 
+		StopProfiler();
+		sprintf(TxBuffer," %lu fps", (unsigned long)(1000000 /fractionaltimeunits) );
+ 		LCD_Text(10, 10, TxBuffer, LCD_Red, LCD_Black);
+
+		}
+	}
+
+	/**
+		* @brief  Configures the different system clocks.
+		* @param  None
+		* @retval : None
+		*/
+	void RCC_Configuration(void)
+	{   
+		/* Setup the microcontroller system. Initialize the Embedded Flash Interface,  
+			 initialize the PLL and update the SystemFrequency variable. */
+		SystemInit();
+	}											
+
+	void GPIO_Configuration(void){
+		GPIO_InitTypeDef GPIO_InitStructure;
+		
+		RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA | RCC_APB2Periph_GPIOB | RCC_APB2Periph_GPIOC |
+													 RCC_APB2Periph_GPIOD | RCC_APB2Periph_GPIOE , ENABLE); 	
+		GPIO_InitStructure.GPIO_Pin = GPIO_Pin_6;				     //D1
+		GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+		GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+		GPIO_Init(GPIOC, &GPIO_InitStructure);					 
+		
+		GPIO_InitStructure.GPIO_Pin = GPIO_Pin_13 |GPIO_Pin_6|GPIO_Pin_3;		 //
+		GPIO_Init(GPIOD, &GPIO_InitStructure);
+
+		GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0;		 //
+		GPIO_Init(GPIOE, &GPIO_InitStructure);
+		
+		GPIO_InitStructure.GPIO_Pin = GPIO_Pin_1 ; 	 //LCD-RST
+		GPIO_Init(GPIOE, &GPIO_InitStructure);  	
+		
+	 
+		GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0 | GPIO_Pin_1 | GPIO_Pin_4 | GPIO_Pin_5 |
+																	GPIO_Pin_8 | GPIO_Pin_9 | GPIO_Pin_10 | GPIO_Pin_14 | 
+																	GPIO_Pin_15;
+		GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+		GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
+		GPIO_Init(GPIOD, &GPIO_InitStructure);
+
+		/* Set PE.07(D4), PE.08(D5), PE.09(D6), PE.10(D7), PE.11(D8), PE.12(D9), PE.13(D10),
+			 PE.14(D11), PE.15(D12) as alternate function push pull */
+		GPIO_InitStructure.GPIO_Pin = GPIO_Pin_7 | GPIO_Pin_8 | GPIO_Pin_9 | GPIO_Pin_10 | 
+																	GPIO_Pin_11 | GPIO_Pin_12 | GPIO_Pin_13 | GPIO_Pin_14 | 
+																	GPIO_Pin_15;
+		GPIO_Init(GPIOE, &GPIO_InitStructure); 
+
+		
+		/* NE1 configuration */
+		GPIO_InitStructure.GPIO_Pin = GPIO_Pin_7; 
+		GPIO_Init(GPIOD, &GPIO_InitStructure);
+		
+		/* RS */
+		GPIO_InitStructure.GPIO_Pin = GPIO_Pin_11 ; 	  //RS
+		GPIO_Init(GPIOD, &GPIO_InitStructure); 
+		
+		/* RST */
+	 	
+		GPIO_SetBits(GPIOD, GPIO_Pin_7);			//CS=1 
+		GPIO_SetBits(GPIOD, GPIO_Pin_14| GPIO_Pin_15 |GPIO_Pin_0 | GPIO_Pin_1);  //µÍ8Î»	 
+		GPIO_SetBits(GPIOE, GPIO_Pin_7 | GPIO_Pin_8 | GPIO_Pin_9 | GPIO_Pin_10); //µÍ8Î»  
+		GPIO_ResetBits(GPIOE, GPIO_Pin_0);
+		GPIO_ResetBits(GPIOE, GPIO_Pin_1);		//RESET=0
+		GPIO_SetBits(GPIOD, GPIO_Pin_4);		    //RD=1
+		GPIO_SetBits(GPIOD, GPIO_Pin_5);			//WR=1
+		
+		
+		
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_6|GPIO_Pin_7;		//D1  D2
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+  GPIO_Init(GPIOC, &GPIO_InitStructure);					 
+
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_6|GPIO_Pin_13;		 //D3, D4
+  GPIO_Init(GPIOD, &GPIO_InitStructure);
+ 
+	 }
+
+ 
+
+	/**
+		 * @brief  Fill the global buffer
+		* @param pBuffer: pointer on the Buffer to fill
+		* @param BufferSize: size of the buffer to fill
+		* @param Offset: first value to fill on the Buffer
+		*/
+
+	#ifdef  USE_FULL_ASSERT
+
+	/**
+		* @brief  Reports the name of the source file and the source line number
+		*   where the assert_param error has occurred.
+		* @param file: pointer to the source file name
+		* @param line: assert_param error line source number
+		* @retval : None
+		*/
+	void assert_failed(uint8_t* file, uint32_t line)
+	{ 
+		/* User can add his own implementation to report the file name and line number,
+			 ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+
+		/* Infinite loop */
+		while (1)
+		{
+		}
+	}
+	#endif
+	 
